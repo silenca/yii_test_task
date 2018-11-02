02fcd0db02a23982c62956dd9c7e65c36342cc23
andyshow <sholomko.andy@gmail.com>
2018-11-02 11:36:09 +0200

    merge

---
+++ node/config.js
@@ -0,0 +1,20 @@
+exports.mysql = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'yiitt'
+};
+
+exports.crm = {
+    host: 'testcrm.silencatech.com',
+    port: 80
+};
+
+exports.app = {
+    port: 8002
+};
+
+exports.socket = {
+    port: 8005
+};
+
