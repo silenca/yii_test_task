4fec7ab6f078065a92ba94ea3282a6b9c3f7be94
ahwd <austin.haward@gmail.com>
2018-12-06 19:52:40 +0200

    api medium rules

--- web/media/js/global_contact_form.js
+++ web/media/js/global_contact_form.js
@@ -377,12 +377,21 @@ function manageContactFormPermissions(userRole) {
 }
 
 $('div.cs-options li[data-value=call]').click(function () {
-    login();
+    // login();
     $('.btn-audio-call').click(function () {
+        login();
         $('.audio-call-messages').show();
+        // $('.audio-call-messages').show();
+        // var phone_number = $('#contact_phones').val();
+        // if (phone_number.startsWith('+380')) {
+        //     phone_number = phone_number.slice(3);
+        // } else if (phone_number.startsWith('0') && phone_number.length !== 10) {
+        //     phone_number = phone_number.trim();
+        // }
+        // makeCall(phone_number);
         $('.acb-call-btn').click(function (e) {
-            $(this).disable();
-            $('.acb-hang-up-btn').enable();
+            $(this).attr('disabled', true);
+            $('.acb-hang-up-btn').attr('disabled', false);
             var phone_number = $('#contact_phones').val();
             if (phone_number.startsWith('+380')) {
                 phone_number = phone_number.slice(3);
@@ -390,12 +399,14 @@ $('div.cs-options li[data-value=call]').click(function () {
                 phone_number = phone_number.trim();
             }
             makeCall(phone_number);
+            $('.acb-hang-up-btn').click(function(){
+
+                $(this).attr('disabled', true);
+                $('.acb-call-btn').attr('disabled', false);
+                hangUp();
+            });
         })
-        $('.acb-hang-up-btn').click(function(){
-            $(this).disable();
-            $('.acb-call-btn').enable();
-            callSession.hangup();
-        });
+
     })
 });
 function fillContactData(data, $form) {
