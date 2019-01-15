98ceb744f128eff6b2a4166f01d0df49c86f827c
Kachan Sergey <s.kachan@silencatech.com>
2019-01-15 09:53:11 +0200

    Fix hideNotification call

--- web/media/js/notifications.js
+++ web/media/js/notifications.js
@@ -1,24 +1,24 @@
- $(function(){
-     //var socket = io.connect('wss://dopomogaplus.silencatech.com:8005');
-
-     /*
-      * message: text
-      * position: top | bottom | top-left | top-right | bottom-left | bottom-right
-      * type: info | warning | success | danger | default
-      * style: bar | flip | circle | simple
-      */
-     function showNotification(selector, message, position, type, style, timeout) {
-         if (!timeout) timeout = 0;
-         $(selector).pgNotification({
-             style: style,
-             message: message,
-             position: position,
-             timeout: timeout,
-             type: type
-         }).show();
-     }
-
-     function hideNotifications(selector) {
-         selector.find('.pgn').remove();
-     }
+$(function () {
+    //var socket = io.connect('wss://dopomogaplus.silencatech.com:8005');
 });
+
+/*
+ * message: text
+ * position: top | bottom | top-left | top-right | bottom-left | bottom-right
+ * type: info | warning | success | danger | default
+ * style: bar | flip | circle | simple
+ */
+function showNotification(selector, message, position, type, style, timeout) {
+    if (!timeout) timeout = 0;
+    $(selector).pgNotification({
+        style: style,
+        message: message,
+        position: position,
+        timeout: timeout,
+        type: type
+    }).show();
+};
+
+function hideNotifications(selector) {
+    selector.find('.pgn').remove();
+};
