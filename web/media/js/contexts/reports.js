060e7e2dbadfcf8e6516eb01f58047f9755900fa
Kachan Sergey <s.kachan@silencatech.com>
2019-01-29 10:54:25 +0200

    Fix reports table filters

--- web/media/js/contexts/reports.js
+++ web/media/js/contexts/reports.js
@@ -1,13 +1,4 @@
 var currentPage = "reports";
-var dataTable;
-// var incoming = 0;
-// var outgoing = 0;
-// var leads = 0;
-var tag_filters = [];
-var start_date_filter = null;
-var end_date_filter = null;
-var user_filter = null;
-var attraction_channel = null;
 
 var date = new Date();
 var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
@@ -17,20 +8,12 @@ var firstDayFormatted = (firstDay.getDate()) + '/' + (firstDay.getMonth() + 1) +
 var lastDayFormatted = (lastDay.getDate()) + '/' + (lastDay.getMonth() + 1) + '/' + lastDay.getFullYear();
 
 $(function () {
-
-
-    var initTable = function () {
-        var table = $('#reports-table');
-
+    var initTable = function(wr) {
         var settings = {
             "sDom": "<'table-responsive't><'row'<p i>>",
             "sPaginationType": "bootstrap",
             "destroy": true,
             "scrollCollapse": true,
-//            "oLanguage": {
-//                "sLengthMenu": "_MENU_ ",
-//                "sInfo": "Showing <b>_START_ to _END_</b> of _TOTAL_ entries"
-//            },
             "info": false,
             "iDisplayLength": 20,
             "paging": false,
@@ -38,78 +21,50 @@ $(function () {
             "serverSide": true,
             "order": [],
             "ajax": {
-                url: "/reports/getdata", // json datasource
-                type: "get", // method  , by default get
-                error: function () {  // error handling
-                    //alert('error data');
-                }
+                url: "/reports/getdata",
+                type: "get",
+                error: function(){}
             },
             "columnDefs": [
                 {"targets": 0, "orderable": false},
                 {"targets": 1, "orderable": false},
                 {"targets": 2, "orderable": false},
                 {"targets": 3, "orderable": false},
-                /*{
-                    "targets": 3,
-                    "orderable": false,
-                    "createdCell": function (td, cellData, rowData, row, col) {
-                        var tags = '';
-                        cellData.forEach(function (val) {
-                            var tagClass = 'tag label';
-                            if (val['is_deleted'] == 1) {
-                                tagClass += ' label-danger';
-                            }
-                            tags += '<div class="' + tagClass + '" data-id="' + val['tag_id'] + '">' + val['name'] + '</div>';
-                        });
-                        $(td).html("<div class='tags_block clearfix'>" + tags + "</div>");
-                    }
-                },*/
                 {"targets": 4, "orderable": false},
                 {"targets": 5, "orderable": false},
             ],
-            "createdRow": function (row, data, index) {
-
-                // incoming += parseInt(data[1]);
-                // outgoing += parseInt(data[2]);
-                // leads += parseInt(data[3]);
-            },
-            "drawCallback": function () {
-                tag_filters.forEach(function (val) {
-                    $('.tag[data-id=' + val + ']').addClass('label-success');
-                });
-
-            }
+            "createdRow": function(row, data, index){},
+            "drawCallback": function(){}
         };
-        dataTable = table.DataTable(settings);
-
-        $('.search-input-select').on('change', function () {   // for select box
-            user_filter = $(this).val();
-            user_filter ? dataTable.columns(0).search(user_filter) : null;
-            start_date_filter ? dataTable.columns(1).search(start_date_filter) : null;
-            end_date_filter ? dataTable.columns(2).search(end_date_filter) : null;
-            tag_filters ? dataTable.columns(3).search(tag_filters) : null;
-			attraction_channel ? dataTable.columns(4).search(attraction_channel) : null;
-            dataTable.draw();
-        });
-
-        $('.search-input-select-two').on('change', function () {   // for select box
-			attraction_channel = $(this).val();
-            user_filter ? dataTable.columns(0).search(user_filter) : null;
-            start_date_filter ? dataTable.columns(1).search(start_date_filter) : null;
-            end_date_filter ? dataTable.columns(2).search(end_date_filter) : null;
-            tag_filters ? dataTable.columns(3).search(tag_filters) : null;
-			attraction_channel ? dataTable.columns(4).search(attraction_channel) : null;
-            dataTable.draw();
-        });
+        return $(wr).DataTable(settings);
     };
 
-    var resetVars = function () {
-        //incoming = outgoing = leads = 0;
-    };
-
-    if ($('#reports-table').length) {
-        initTable();
+    if(!$('#reports-table').length) {
+        return;
     }
+
+    var dataTable = initTable('#reports-table');
+
+    var activeFilters = {};
+    var filtersMap = {user: 0, start: 1, end: 2, tag: 3, attrChannel: 4};
+    var updateTable = _.throttle(function(){
+        _.each(activeFilters, function(value, name){
+            if(value) {
+                dataTable.columns(filtersMap[name]).search(value);
+            }
+        });
+        dataTable.draw();
+    }, 200, { leading: false });
+
+    $('select.search-input-select').on('change', function(){
+        activeFilters.user = $(this).val();
+        updateTable();
+    });
+
+    $('select.search-input-select-two').on('change', function(){
+        activeFilters.attrChannel = $(this).val();
+        updateTable();
+    });
 
     $('input[name="daterange"]').daterangepicker(
         {
@@ -160,30 +115,9 @@ $(function () {
                 'За всё время': ['01.05.15', moment()]
             }
         }, function (start, end, label) {
-            start_date_filter = start.format('YYYY-MM-DD HH:mm:ss');
-            end_date_filter = end.format('YYYY-MM-DD HH:mm:ss');
-            user_filter ? dataTable.columns(0).search(user_filter) : null;
-            start_date_filter ? dataTable.columns(1).search(start_date_filter) : null;
-            end_date_filter ? dataTable.columns(2).search(end_date_filter) : null;
-            tag_filters ? dataTable.columns(3).search(tag_filters) : null;
-			attraction_channel ? dataTable.columns(4).search(attraction_channel) : null;
-            dataTable.draw();
+            activeFilters.start = start.format('YYYY-MM-DD HH:mm:ss');
+            activeFilters.end = end.format('YYYY-MM-DD HH:mm:ss');
+            updateTable();
         }
     );
-
-    $('table').on('click', '.tag', function () {
-        //$(this).toggleClass('label-success');
-        if (tag_filters.indexOf($(this).data('id')) == -1) {
-            tag_filters.push($(this).data('id'));
-        } else {
-            tag_filters.splice(tag_filters.indexOf($(this).data('id')), 1);
-        }
-
-        user_filter ? dataTable.columns(0).search(user_filter) : null;
-        start_date_filter ? dataTable.columns(1).search(start_date_filter) : null;
-        end_date_filter ? dataTable.columns(2).search(end_date_filter) : null;
-        tag_filters ? dataTable.columns(3).search(tag_filters) : null;
-		attraction_channel ? dataTable.columns(4).search(attraction_channel) : null;
-        dataTable.draw();
-    });
 });
